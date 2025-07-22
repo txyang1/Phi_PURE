646200e86e4f3b8a87f8ac6ea6b6ef6f361dbdff
txyang1 <tingxinyang2@gamil.com>
2025-07-22 21:52:26 +0200

    tx add 0722

--- tx_submit.sh
+++ tx_submit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=phi_pure_rewardfree_1.5b
+#SBATCH --job-name=phi_pure_rewardfree0722_1.5b
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
 #SBATCH --gres=gpu:4
 #SBATCH --partition=accelerated
-#SBATCH --time=24:00:00
+#SBATCH --time=06:00:00
 #SBATCH --output=logs/run1_%j.out
 #SBATCH --error=logs/run1_%j.err
 
@@ -16,7 +16,7 @@ mkdir -p logs
 export OMP_NUM_THREADS=32
 
 export WANDB_MODE=online
-export WANDB_PROJECT=phi_pure_rloo_rewardfree_1,5b
-export WANDB_NAME=phi_pure_rloo_rewardfree_1.5b_run1
+export WANDB_PROJECT=phi_pure_rloo_rewardfree0722_1,5b
+export WANDB_NAME=phi_pure_rloo_rewardfree0722_1.5b_run1
 
 python verl/trainer/main_ppo.py 
