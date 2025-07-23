a1f61ff613e16578683226d5dab67f2cba5a3fb5
txyang1 <tingxinyang2@gamil.com>
2025-07-23 19:37:20 +0200

    change to workspace

--- submit_pure.sh
+++ submit_pure.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --gres=gpu:4
 #SBATCH --partition=accelerated
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH --output=logs/run1_%j.out
 #SBATCH --error=logs/run1_%j.err
 
